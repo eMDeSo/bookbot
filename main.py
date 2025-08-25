c5862f1f12a38097c9f7a1bd7217975dbde8e9df
eMDeSo <dominoday222@gmail.com>
2025-08-25 16:05:40 +0200

    added sorting func to stats and refactored main for readability

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, dict_to_list
 
 def get_books_text(filepath):
     with open(filepath) as f:
@@ -6,9 +6,18 @@ def get_books_text(filepath):
     return contents
 
 def main():
-    book_text = get_books_text("./books/frankenstein.txt")
-    print(f"{count_words(book_text)} words found in the document")
-    print(count_characters(book_text))
-
-
+    filepath = "./books/frankenstein.txt"
+    book_text = get_books_text(filepath)
+    words_count = count_words(book_text)
+    char_dict = count_characters(book_text)
+    sorted_char_list = dict_to_list(char_dict)
+    #print(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -9,6 +9,16 @@ def count_characters(text_data):
             char_dict[char] += 1
         else:
             char_dict[char] = 1
-
     return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def dict_to_list(dictionary):
+    list_of_dictionaries = []
+    for key, value in dictionary.items():
+        if key.isalpha():
+            list_of_dictionaries.append({"char": key, "num": value})
+    list_of_dictionaries.sort(reverse=True, key=sort_on)
+    return list_of_dictionaries
 
